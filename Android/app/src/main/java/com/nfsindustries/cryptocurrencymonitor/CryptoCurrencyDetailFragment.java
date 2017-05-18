7b185d009266eea2ce6d127abe20af9dc8527cec
Natan Facchin <maxmousee@gmail.com>
2017-05-18 17:37:52 -0300

    Created currency data model

--- Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/CryptoCurrencyDetailFragment.java
+++ Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/CryptoCurrencyDetailFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import com.nfsindustries.cryptocurrencymonitor.dummy.DummyContent;
+import com.nfsindustries.cryptocurrencymonitor.model.CurrencyModel;
 
 /**
  * A fragment representing a single CryptoCurrency detail screen.
@@ -27,7 +27,7 @@ public class CryptoCurrencyDetailFragment extends Fragment {
     /**
      * The dummy content this fragment is presenting.
      */
-    private DummyContent.DummyItem mItem;
+    private CurrencyModel.CurrencyItem mItem;
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -44,7 +44,7 @@ public class CryptoCurrencyDetailFragment extends Fragment {
             // Load the dummy content specified by the fragment
             // arguments. In a real-world scenario, use a Loader
             // to load content from a content provider.
-            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+            mItem = CurrencyModel.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
 
             Activity activity = this.getActivity();
             CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
--- Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/model/CurrencyModel.java
+++ Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/model/CurrencyModel.java
@@ -1,4 +1,4 @@
-package com.nfsindustries.cryptocurrencymonitor.dummy;
+package com.nfsindustries.cryptocurrencymonitor.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -6,39 +6,43 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * Created by maxmouse on 18/5/2017.
+ */
+
+public class CurrencyModel {
+
+/**
  * Helper class for providing sample content for user interfaces created by
  * Android template wizards.
  * <p>
- * TODO: Replace all uses of this class before publishing your app.
  */
-public class DummyContent {
 
     /**
      * An array of sample (dummy) items.
      */
-    public static final List<DummyItem> ITEMS = new ArrayList<DummyItem>();
+    public static final List<CurrencyItem> ITEMS = new ArrayList<CurrencyItem>();
 
     /**
-     * A map of sample (dummy) items, by ID.
+     * A map of sample  items, by ID.
      */
-    public static final Map<String, DummyItem> ITEM_MAP = new HashMap<String, DummyItem>();
+    public static final Map<String, CurrencyItem> ITEM_MAP = new HashMap<String, CurrencyItem>();
 
     private static final int COUNT = 25;
 
     static {
         // Add some sample items.
         for (int i = 1; i <= COUNT; i++) {
-            addItem(createDummyItem(i));
+            addItem(createCurrencyItem(i));
         }
     }
 
-    private static void addItem(DummyItem item) {
+    private static void addItem(CurrencyItem item) {
         ITEMS.add(item);
         ITEM_MAP.put(item.id, item);
     }
 
-    private static DummyItem createDummyItem(int position) {
-        return new DummyItem(String.valueOf(position), "Item " + position, makeDetails(position));
+    private static CurrencyItem createCurrencyItem(int position) {
+        return new CurrencyItem(String.valueOf(position), "Item " + position, makeDetails(position));
     }
 
     private static String makeDetails(int position) {
@@ -51,14 +55,14 @@ public class DummyContent {
     }
 
     /**
-     * A dummy item representing a piece of content.
+     * A currency item representing a piece of content.
      */
-    public static class DummyItem {
+    public static class CurrencyItem {
         public final String id;
         public final String content;
         public final String details;
 
-        public DummyItem(String id, String content, String details) {
+        public CurrencyItem(String id, String content, String details) {
             this.id = id;
             this.content = content;
             this.details = details;
