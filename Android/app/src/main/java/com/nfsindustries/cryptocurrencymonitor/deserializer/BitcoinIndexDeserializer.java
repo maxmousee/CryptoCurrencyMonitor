e9009e22c566cb7553517f078ea3ba6c99422220
Natan Facchin <maxmousee@gmail.com>
2017-05-24 15:44:14 -0300

    Code style improvements

--- Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/deserializer/BitcoinIndexDeserializer.java
+++ Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/deserializer/BitcoinIndexDeserializer.java
@@ -18,7 +18,10 @@ import com.nfsindustries.cryptocurrencymonitor.utils.Constants;
 public class BitcoinIndexDeserializer implements JsonDeserializer<BitcoinModel> {
 
     @Override
-    public BitcoinModel deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+    public BitcoinModel deserialize(final JsonElement json, final Type typeOfT,
+                                    final JsonDeserializationContext context)
+            throws JsonParseException {
+        
         final JsonObject bitcoinIndexJsonObject =  json.getAsJsonObject();
         final JsonObject usdJsonObject = bitcoinIndexJsonObject.getAsJsonObject(Constants.USD_KEY);
         final float last15m = usdJsonObject.get(Constants.LAST_15M_KEY).getAsFloat();
