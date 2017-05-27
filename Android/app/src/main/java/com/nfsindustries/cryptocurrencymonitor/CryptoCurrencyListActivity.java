b0e14cdb6bc9dfef75ccdc02e0b397e0129e8884
Natan Facchin <maxmousee@gmail.com>
2017-05-26 22:34:17 -0300

    Removed unused imports

--- Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/CryptoCurrencyListActivity.java
+++ Android/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/CryptoCurrencyListActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.nfsindustries.cryptocurrencymonitor.model.CurrencyListItem;
