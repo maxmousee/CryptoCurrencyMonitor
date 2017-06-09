55e25dbc35497a65f4a625395acb29a36bda7246
Natan Facchin <maxmousee@gmail.com>
2017-06-09 10:07:25 -0300

    Added automated UI tests

--- Android/app/src/androidTest/java/com/nfsindustries/cryptocurrencymonitor/InstrumentedTest.java
+++ Android/app/src/androidTest/java/com/nfsindustries/cryptocurrencymonitor/InstrumentedTest.java
@@ -2,11 +2,17 @@ package com.nfsindustries.cryptocurrencymonitor;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
 
 /**
@@ -16,11 +22,20 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class InstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<CryptoCurrencyListActivity> mActivityRule = new ActivityTestRule(CryptoCurrencyListActivity.class);
+
+    @Test
+    public void bitcoinIsDisplayedOnView() {
+        onView(withText("bitcoin")).check(matches(isDisplayed()));
+        onView(withText("dogecoin")).check(matches(isDisplayed()));
+    }
+
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("com.nfsindustries.cryptocurrencymonitor", appContext.getPackageName());
     }
 }
