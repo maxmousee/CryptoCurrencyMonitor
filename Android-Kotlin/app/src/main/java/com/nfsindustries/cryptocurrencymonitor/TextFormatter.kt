f244f3a61dea57a9cd3fd11bee5789f02ca51549
Natan Facchin <maxmousee@gmail.com>
2017-09-20 05:59:50 -0400

    Added text formatter and cryptocurrency model

---
+++ Android-Kotlin/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/TextFormatter.kt
@@ -0,0 +1,17 @@
+package com.nfsindustries.cryptocurrencymonitor.cryptocurrencymonitor
+
+/**
+ * Static class used to format text, strings and currencies to display
+ */
+
+object TextFormatter {
+
+    /**
+     * Format currency to display with only 2 decimal places
+     * @param value currency value
+     * @return formatted String to display
+     */
+    fun formatCurrency(value: Float): String {
+        return String.format("%.2f", value)
+    }
+}
---
+++ Android-Kotlin/app/src/main/java/com/nfsindustries/cryptocurrencymonitor/model/CryptoCurrencyModel.kt
@@ -0,0 +1,73 @@
+package com.nfsindustries.cryptocurrencymonitor.model
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+import com.nfsindustries.cryptocurrencymonitor.cryptocurrencymonitor.TextFormatter
+
+class CryptoCurrencyModel {
+
+    @SerializedName("id")
+    @Expose
+    var id: String? = null
+    @SerializedName("name")
+    @Expose
+    var name: String? = null
+    @SerializedName("symbol")
+    @Expose
+    var symbol: String? = null
+    @SerializedName("rank")
+    @Expose
+    var rank: Int = 0
+    @SerializedName("price_usd")
+    @Expose
+    var priceUsd: Float = 0.toFloat()
+    @SerializedName("price_btc")
+    @Expose
+    var priceBtc: Float = 0.toFloat()
+    @SerializedName("24h_volume_usd")
+    @Expose
+    var _24hVolumeUsd:Float = 0.toFloat()
+    @SerializedName("market_cap_usd")
+    @Expose
+    var marketCapUsd: Float = 0.toFloat()
+    @SerializedName("available_supply")
+    @Expose
+    var availableSupply: Float = 0.toFloat()
+    @SerializedName("total_supply")
+    @Expose
+    var totalSupply: Float = 0.toFloat()
+    @SerializedName("percent_change_1h")
+    @Expose
+    var percentChange1h: Float = 0.toFloat()
+    @SerializedName("percent_change_24h")
+    @Expose
+    var percentChange24h: Float = 0.toFloat()
+    @SerializedName("percent_change_7d")
+    @Expose
+    var percentChange7d: Float = 0.toFloat()
+    @SerializedName("last_updated")
+    @Expose
+    var lastUpdated: Long = 0
+
+    override fun toString(): String {
+        val displayStrBuilder = StringBuilder("1 ")
+        displayStrBuilder.append(name)
+        displayStrBuilder.append(" to ")
+        displayStrBuilder.append("USD")
+        displayStrBuilder.append("\n\n")
+        displayStrBuilder.append("Last: ")
+        displayStrBuilder.append(TextFormatter.formatCurrency(priceUsd))
+        displayStrBuilder.append("\n\n")
+        displayStrBuilder.append("Change 1h: ")
+        displayStrBuilder.append(percentChange1h)
+        displayStrBuilder.append("%\n\n")
+        displayStrBuilder.append("Change 24h: ")
+        displayStrBuilder.append(percentChange24h)
+        displayStrBuilder.append("%\n\n")
+        displayStrBuilder.append("Change 7d: ")
+        displayStrBuilder.append(percentChange7d)
+        displayStrBuilder.append("%\n\n")
+        return displayStrBuilder.toString()
+    }
+
+}
